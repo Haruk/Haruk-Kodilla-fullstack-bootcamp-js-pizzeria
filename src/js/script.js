916fa90d750cb311037562e544fa770c7f489113
Aleksandra <haruka.lex@gmail.com>
2023-01-01 16:34:46 +0100

    Add script in app.initMenu and app.initData. Add class Product and render products on site.

--- src/js/script.js
+++ src/js/script.js
@@ -53,15 +53,59 @@
   };
 
   const app = {
+    initMenu(){
+      const thisApp = this;
+      console.log('thisApp.data:',thisApp.data);
+      for(let productData in thisApp.data.products){
+        new Product (productData, thisApp.data.products[productData]);
+      }
+    },
+    
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+    
     init: function(){
       const thisApp = this;
+
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
+
+  class Product {
+    constructor (id, data){
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
+      console.log('new Product:', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      // find menu container
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      
+      menuContainer.appendChild(thisProduct.element);
+    }
+  };
+  
+
+
 
   app.init();
 }
